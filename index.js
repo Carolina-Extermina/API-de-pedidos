8235f249fb02d6724472bdf16982a0d83290de3d
Carolina Machado <159204512+Carolina-Extermina@users.noreply.github.com>
2026-03-09 23:15:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-// Configurações
 app.use(cors());
-app.use(express.json()); // Para ler o JSON do body
+app.use(express.json()); 
 
-// Rotas
 app.use('/order', orderRoutes);
 
-// Middleware de Erros Global
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+
 });
