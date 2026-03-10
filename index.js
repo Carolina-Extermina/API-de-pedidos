f2ac80f7bc7ca412ed722d649321a1c59a2c3fd3
Carolina Machado <159204512+Carolina-Extermina@users.noreply.github.com>
2026-03-10 00:07:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const orderRoutes = require('./routes/orderRoutes');
 const errorHandler = require('./middleware/errorHandler');
+const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 
+
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 app.use('/order', orderRoutes);
 
@@ -15,5 +17,4 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-
 });
